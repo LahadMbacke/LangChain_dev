7730d3b174dfd407ecec1352d950f4356f64e896
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-05-20 19:35:01 +0200

    Generate response after the retriever

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 LANGUAGE_MODEL = "gpt-3.5-turbo"
 
+model = ChatOpenAI()
+
+
 template:str = """/
 Vous êtes un spécialiste du support client. Vous assistez les utilisateurs avec des demandes générales basées sur {context} et des problèmes techniques. /
 Si vous ne connaissez pas la réponse, vous invitez l'utilisateur à joindre le service support au téléphone ou par mail 
@@ -26,27 +29,41 @@ Si vous ne connaissez pas la réponse, vous invitez l'utilisateur à joindre le 
 system_messages_prompt = SystemMessagePromptTemplate.from_template(template) # permet de définir le template pour les messages du système
 human_messages_prompt = HumanMessagePromptTemplate.from_template(
     input_variable=["question", "context"], 
-    template = "{question}"
+    template = "{question}",
 ) # permet de définir le template pour les messages de l'utilisateur
+
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [system_messages_prompt,human_messages_prompt]
+    ) # permet de définir le template pour les messages de l'utilisateur et du système
+
 
 def load_file():
     loader = TextLoader("./docs/faq_fr.txt")
     text_spliter = CharacterTextSplitter(chunk_size=100,chunk_overlap=0) # permet de découper le texte en morceaux de 1000 caractères
     documents = loader.load()
     chunks = text_spliter.split_documents(documents)
-    # print(f"Number of documents {len(documents)},Number of chunks: {len(chunks)}")
     return chunks
 
 
 def load_embeddings(doucuments,user_query):
     db = Chroma.from_documents(doucuments,OpenAIEmbeddings()) # permet de charger les embeddings des documents 
     docs = db.similarity_search(user_query) # permet de chercher les documents les plus similaires à la requête de l'utilisateur
-    print(f"Similarity search result: {docs}")
     return db.as_retriever()
+
+def generate_response(retriever,query):
+    chain =(
+        {"context":retriever,"question":RunnablePassthrough()}
+        | chat_prompt_template
+        | model
+        | StrOutputParser()
+    )
+    return chain.invoke(query)
+
 
 def query(query):
    documents =  load_file()
-   retriver = load_embeddings(documents,query) # permet de charger les embeddings des documents et de chercher les documents les plus similaires à la requête de l'utilisateur
-   
-query("Quelle est la capital du Senegal ?")
-# query("Quelle est la politique de remboursement ?")
+   retriever = load_embeddings(documents,query) # charger les embeddings des documents et de chercher les documents les plus similaires à la requête de l'utilisateur
+   response = generate_response(retriever,query)
+   print(Fore.GREEN + response)  
+
+query("Quelle est la politique de remboursement ?")
