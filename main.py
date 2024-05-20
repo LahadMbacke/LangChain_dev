1494b89b2f8b435a7232cad5830ffaeeb4379906
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-05-20 16:29:58 +0200

    Talking a Joke about a topic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate 
-from langchain.output_parsers import StrOutputParser 
+from langchain.schema.output_parser import StrOutputParser
 # from langchain_core.runnables import RunnablePassthrough # type: ignore
 
 from dotenv import load_dotenv
@@ -12,10 +12,34 @@ load_dotenv()
 def format_response(result):
     print(Fore.GREEN + result.content + Fore.RESET)
 
-model = ChatOpenAI(model = "gpt-3.5-turbo")
-prompt = ChatPromptTemplate.from_template("Tel me a joke about {topic}")
+def model_chain(topic):
+    model = ChatOpenAI(model = "gpt-3.5-turbo")
+    prompt = ChatPromptTemplate.from_template("Tel me a joke about {topic}")
 
-chain = prompt | model | format_response
+    chain = prompt | model | StrOutputParser()
+    return chain.invoke({"topic": input})
 
-chain.invoke({"topic": "Liverpool FC"})
+# chain.invoke({"topic": "Liverpool FC"})
 
+def main():
+    print("===========Menu============")
+    print("[1]: If you want a joke")
+    print("[2]: If you want to exit")
+    print("============================")
+    choix = input("Enter your choice: ")
+
+    if choix == "1":
+        ask()
+    elif choix == "2":
+        print("Goodbye!")
+        exit()
+
+def ask():
+    input_user = input("Give me a topic: ")
+    print("Thinking...")
+    print(Fore.GREEN + model_chain(input_user) + Fore.RESET)
+
+
+
+if __name__ == "__main__":
+    main()
