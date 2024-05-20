e0c43cfc4e5ee748ff7e51c4d90c299159a5f7d2
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-05-20 16:01:41 +0200

    template and forma reponse

---
+++ main.py
@@ -0,0 +1,21 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate 
+from langchain.output_parsers import StrOutputParser 
+# from langchain_core.runnables import RunnablePassthrough # type: ignore
+
+from dotenv import load_dotenv
+import os
+from colorama import Fore
+
+load_dotenv()
+
+def format_response(result):
+    print(Fore.GREEN + result.content + Fore.RESET)
+
+model = ChatOpenAI(model = "gpt-3.5-turbo")
+prompt = ChatPromptTemplate.from_template("Tel me a joke about {topic}")
+
+chain = prompt | model | StrOutputParser()
+
+chain.invoke({"topic": "Liverpool FC"})
+
